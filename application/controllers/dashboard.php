a52edfc2901c6daf979ea0d320e64f0f4c00ccfe
Jamess-2209 <jamestauhid123@gmail.com>
2021-12-15 10:38:35 +0700

    Grayscale using CI3

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,14 @@
+<?php 
+class dashboard extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('dashboard');
+        $this->load->view('templates/footer');
+    }
+}
+
+
+?>
